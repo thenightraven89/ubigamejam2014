e444705543cc89e3999241ea05b3a4c9e4891e5f
Raul Incze <raulincze@gmail.com>
2014-11-22 14:12:36 +0200

    Added scripts folder. Added GameManager and abstract GameState class.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	private static GameManager instance;
+	private GameState currentState;
+
+	public static GameManager Instance {get{return instance;}}
+	public GameState State 
+	{
+		get { return currentState; }
+		set
+		{
+			SwitchState(value);
+		}
+	}
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	private void SwitchState(GameState state)
+	{
+		if(currentState != null)
+		{
+			currentState.TransitionOut();
+		}
+		currentState = state;
+		currentState.TransitionIn();
+	}
+}
---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class GameState
+{
+	public abstract void TransitionIn();
+	public abstract void TransitionOut();
+}
