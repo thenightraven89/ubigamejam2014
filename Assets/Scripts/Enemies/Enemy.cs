e30abe4f5f5e3f94c60cb897e856059655888b06
Raul Incze <raulincze@gmail.com>
2014-11-22 15:34:47 +0200

    Enemy roaming behavior added.

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -2,14 +2,62 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
-	public Transform navTarget;
+	[HideInInspector]
+	public Transform playerPrefab;
+	public float walkSpeed = 2.5f;
+	public float runSpeed = 4.0f;
+	public float maximumRoamRange = 1.5f;
+	public float minimumRoamRange = -1.5f;
+	public float roamMinTime = 0.5f;
+	public float roamMaxTime = 2.0f;
+	private Vector3 targetPosition;
+	private float roamWaitTime;
+	private EnemyState state;
+	private NavMeshAgent agent;
 	// Use this for initialization
-	void Start () {
-		GetComponent<NavMeshAgent>().SetDestination(navTarget.position);
+	void Start () 
+	{
+		agent = GetComponent<NavMeshAgent>();
+		SetState(EnemyState.Idle);
+		StartCoroutine(ChangeRoamPosition());
 	}
-	
+
+	IEnumerator ChangeRoamPosition()
+	{
+		yield return new WaitForSeconds(Random.Range(2f, 4f));
+		while(true)
+		{
+			roamWaitTime = Random.Range (roamMinTime, roamMaxTime);
+			if(state == EnemyState.Idle)
+			{
+				targetPosition = transform.position + new Vector3(Random.Range (minimumRoamRange,maximumRoamRange), 
+				                             					  0, 
+				                                                  Random.Range (minimumRoamRange,maximumRoamRange));
+				
+				agent.SetDestination(targetPosition);
+			}
+			yield return new WaitForSeconds(roamWaitTime);
+		}
+	}
+
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+
+	}
+
+	void SetState(EnemyState newState)
+	{
+		if(newState == EnemyState.Idle)
+			agent.speed = walkSpeed;
+		if(newState == EnemyState.Attacking)
+			agent.speed = runSpeed;
+		state = newState;
+	}
+
+	public enum EnemyState
+	{
+		Idle,
+		Attacking
 	}
 }
