1638af5364638de114508248916698959f8764f4
Raul Incze <raulincze@gmail.com>
2014-11-22 14:43:26 +0200

    AI WIP. Player prefab.

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	public Transform navTarget;
+	// Use this for initialization
+	void Start () {
+		GetComponent<NavMeshAgent>().SetDestination(navTarget.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
