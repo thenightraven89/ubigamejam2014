94673f4ee6b6d0c6e205992932c3f211ffdbd463
pulseman <bogdan.tana@gmail.com>
2014-11-23 03:07:51 +0200

    added spawn points, waves with monster batches

---
+++ Assets/Scripts/MonsterBatch.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterBatch : MonoBehaviour
+{
+    // id of wave that the batch participates in
+    public int waveId;
+
+    // source monster to clone
+    public GameObject sourceObject;
+
+    // amount of monsters of this type participating in this wave
+    public int monsterCount;
+
+    // this will update over time (decrease)
+    private int remainingCount;
+
+    public void Awake()
+    {
+        remainingCount = monsterCount;
+    }
+
+    public bool IsAvailable()
+    {
+        return remainingCount > 0;
+    }
+
+    public void UseMonster()
+    {
+        remainingCount--;
+    }
+}
---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveManager : MonoBehaviour
+{
+    private float minWaitBetweenMobs = 0.25f;
+    private float maxWaitBetweenMobs = 0.75f;
+
+    private int currentWaveId;
+
+    private MonsterBatch[] batches;
+
+    public Transform[] spawners;
+
+    private int spawnerCursor;
+
+    private int waveCount = 1;
+
+    private float waveCooldownTime = 5f;
+
+    // Use this for initialization
+    void Start()
+    {
+        batches = GetComponents<MonsterBatch>();
+
+        currentWaveId = 0;
+
+        spawnerCursor = 0;
+        
+        StartCoroutine("Spawn");
+    }
+
+    private IEnumerator Spawn()
+    {
+        while (currentWaveId < waveCount)
+        {
+            yield return new WaitForSeconds(waveCooldownTime);
+
+            while (!WaveIsFinished(currentWaveId))
+            {
+                SpawnFromWave(currentWaveId);
+                yield return new WaitForSeconds(Random.Range(minWaitBetweenMobs, maxWaitBetweenMobs));
+            }
+
+            Debug.Log("wave ended");
+            
+            currentWaveId++;
+        }
+
+        Debug.Log("THE END");
+    }
+
+    private void SpawnFromWave(int currentWaveId)
+    {
+        for (int i = 0; i < batches.Length; i++)
+        {
+            if (batches[i].waveId == currentWaveId && batches[i].IsAvailable())
+            {
+                spawnerCursor = (spawnerCursor + 1) % spawners.Length;
+                GameObject newMob = GameObject.Instantiate(batches[i].sourceObject, spawners[spawnerCursor].position, Quaternion.identity) as GameObject;
+                newMob.transform.parent = transform;
+                batches[i].UseMonster();
+            }
+        }
+    }
+
+    private bool WaveIsFinished(int currentWaveId)
+    {
+        for (int i = 0; i < batches.Length; i++)
+        {
+            if (batches[i].waveId == currentWaveId && batches[i].IsAvailable())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
