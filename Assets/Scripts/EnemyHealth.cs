12065596b716cbaff932df0bd271523a67e1b2fb
Raul Incze <raulincze@gmail.com>
2014-11-23 06:02:09 +0200

    asddddddddddddddddddddqw

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+	private float initialScale;
+	public Enemy enm;
+	private int initialHP;
+	// Use this for initialization
+	void Start () {
+		initialScale = transform.localScale.x;
+		initialHP = enm.hitPoints;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		if(enm.hitPoints == initialHP)
+		{
+			renderer.enabled = false;
+		}
+		else
+		{
+			renderer.enabled = true;
+			float val = enm.hitPoints * initialScale / initialHP;
+			transform.localScale = new Vector3(val,transform.localScale.y, transform.localScale.z);
+		}
+	}
+}
